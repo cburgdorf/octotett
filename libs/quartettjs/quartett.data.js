b2fd7d6301d48df61e206ac3358b78d5bc77f378
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2012-11-26 22:49:44 +0100

    added more data

--- libs/quartettjs/quartett.data.js
+++ libs/quartettjs/quartett.data.js
@@ -1,3 +1,4 @@
+'use strict';
 
 //Let's derive from the card and do some transformation for displayValue and displayName
 quartett.ProjectCard = function(options) {
@@ -37,7 +38,39 @@ quartett.data = {
                 { contributors: { value: 100 } },
                 { birth: { value: '19.03.2006' }},
                 { _image: { value: 'http://jquery.org/wp-content/uploads/2010/01/JQuery_logo_color_onwhite-300x74.png' }}
+            ]),
+            new quartett.ProjectCard([
+                { name: { value: 'rails'}},
+                { forks: { value: 4288 }},
+                { stars: { value: 16594 }},
+                { contributors: { value: 100 } },
+                { birth: { value: '21.11.2004' }},
+                { _image: { value: 'http://upload.wikimedia.org/wikipedia/en/thumb/e/e9/Ruby_on_Rails.svg/500px-Ruby_on_Rails.svg.png' }}
+            ]),
+            new quartett.ProjectCard([
+                { name: { value: 'git'}},
+                { forks: { value: 975 }},
+                { stars: { value: 3206 }},
+                { contributors: { value: 100 } },
+                { birth: { value: '03.04.2005' }},
+                { _image: { value: 'http://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/500px-Git-logo.svg.png' }}
+            ]),
+            new quartett.ProjectCard([
+                { name: { value: 'nodejs'}},
+                { forks: { value: 2842 }},
+                { stars: { value: 18835 }},
+                { contributors: { value: 100 } },
+                { birth: { value: '15.02.2009' }},
+                { _image: { value: 'http://upload.wikimedia.org/wikipedia/en/a/a7/Nodejs_logo_light.png' }}
+            ]),
+            new quartett.ProjectCard([
+                { name: { value: 'mongodb'}},
+                { forks: { value: 601 }},
+                { stars: { value: 3582 }},
+                { contributors: { value: 89 } },
+                { birth: { value: '14.10.2007' }},
+                { _image: { value: 'http://info.10gen.com/rs/10gen/images/mongodb%20badge.png' }}
             ])
-        ]
+        ];
     }
-}
+};
