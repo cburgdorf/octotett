6cb011a2f09d19d4f0c6f0fcbf5334e31c31ea84
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2013-02-18 15:52:21 +0100

    finished grunt configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,9 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      app: ['dest']
+    },
     sencha_dependencies: {
       app: {
           options : {
@@ -11,6 +14,44 @@ module.exports = function(grunt) {
               isTouch: true,
               printDepGraph: true,
               mode: 'dynMock'
+          }
+      }
+    },
+    uglify: {
+      app: {
+          options: {
+              sourceMap: 'dest/source-map.js',
+              beautify: true,
+              compress: false
+          },
+          files: {
+              'dest/app.min.js': [
+                  'libs/jsdeferred/jsdeferred.nodoc.js',
+                  'libs/quartettjs/quartett.min.js',
+                  'libs/quartettjs/quartett.data.js',
+                  '<%= sencha_dependencies_app %>'
+              ]
+          }
+      }
+    },
+    copy: {
+      app: {
+          files: [
+              //{expand: true, src: ['src/**'], dest: 'dest/', cwd: '../libs/touch-2.1.1/'},
+              {src: ['resources/**'], dest: 'dest/'},
+              {src: ['index.html'], dest: 'dest/'}
+          ],
+          options: {
+              processContent: function(content, filePath) {
+                  if (/index.html/.test(filePath)) {
+                      // remove the ext script
+                      content = content.replace(/<script.*id="microloader".*><\/script>/, '<script src="app.min.js"></script><link rel="stylesheet" href="resources/css/app.css">');
+                      // now update the css location
+                      content = content.replace(/\.\.\/libs\/ext-4.1.1a\//, '');
+                      return content.replace(/app\/app.js/, 'app.min.js');
+                  }
+                  return content;
+              }
           }
       }
     }
@@ -22,11 +63,12 @@ module.exports = function(grunt) {
   //grunt.loadNpmTasks('grunt-sencha-dependencies');
   grunt.loadTasks('grunt-sencha-dependencies-0.3.1');
 
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-    // Default task(s).
-
-
-  grunt.registerTask('dep', ['sencha_dependencies:app']);
+  //grunt.registerTask('default', ['sencha_dependencies:app']);
+  grunt.registerTask('default', ['clean:app', 'sencha_dependencies:app', 'uglify:app', 'copy:app']);
 
 };
 
